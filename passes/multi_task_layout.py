022dc43be5001d4f524fedd4ddf6759f5473f31b
rum-yasuhiro <banana.himuken@gmail.com>
2020-08-04 17:00:11 +0900

    first commit

---
+++ passes/multi_task_layout.py
@@ -0,0 +1,9 @@
+# This code is for qiskit compilation tool
+# origin by Yasuhiro Ohkura
+# 2020 / 08 / 04
+# 
+
+"""Compose several circuits to one circuit Layout based on current calibration data for the backend."""
+
+
+import 
