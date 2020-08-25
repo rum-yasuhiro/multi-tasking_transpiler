a856ea4045605b2b064f1636346305f4ff938411
rum-yasuhiro <banana.himuken@gmail.com>
2020-08-25 11:55:28 +0900

    fixed

--- docs/multi_tasking.py
+++ docs/multi_tasking.py
@@ -6,12 +6,15 @@ from qiskit.dagcircuit import DAGCircuit
 from qiskit.converters import circuit_to_dag, dag_to_circuit
 from qiskit.compiler import transpile
 
+from docs.multi_pm import multi_tasking_pass_manager
+
 
 def multitasking_transpile(multi_circuits: Union[QuantumCircuit, List[QuantumCircuit]],
                            backend=None,
                            backend_properties=None,
                            output_names=None,
-                           layout_method=None,
+                           layout_method: Optional[str] = None,
+                           pass_manager=None,
                            ) -> Union[QuantumCircuit, List[QuantumCircuit]]:
     """Mapping several circuits to single circuit based on calibration for the backend
 
@@ -27,28 +30,6 @@ def multitasking_transpile(multi_circuits: Union[QuantumCircuit, List[QuantumCir
     multi_circuits = multi_circuits if isinstance(
         multi_circuits[0], list) else [multi_circuits]
 
-    if layout_method is not None:
-        if output_names is not None:
-            output_names = _perse_output_names(
-                output_names, len(multi_circuits))
-        circuits = []
-        for i, circuit_set in enumerate(multi_circuits):
-            dag_list = [circuit_to_dag(circ) for circ in circuit_set]
-            new_dag = layout_method.run(dag_list)
-            new_circ = dag_to_circuit(new_dag)
-            """FIXME
-            logical ->physicalのmappingが
-
-            Code: 
-            
-            """
-            new_circ._layout = layout_method.property_set['layout']
-            if isinstance(output_names, list) and output_names[i] is not None:
-                new_circ.name = output_names[i]
-            circuits.append(new_circ)
-        if len(circuits) == 1:
-            return circuits[0]
-        return circuits
     # Get combine_args(mp_args) to configure the circuit combine job(s)
     combine_args = _parse_combine_args(
         multi_circuits, backend, backend_properties, output_names)
@@ -57,9 +38,31 @@ def multitasking_transpile(multi_circuits: Union[QuantumCircuit, List[QuantumCir
     multi_programming_circuit = list(map(
         _compose_multicircuits, list(zip(multi_circuits, combine_args))))
 
-    if len(multi_programming_circuit) == 1:
-        return multi_programming_circuit[0]
-    return multi_programming_circuit
+    if layout_method == 'crosstalk':
+        if output_names is not None:
+            output_names = _perse_output_names(
+                output_names, len(multi_circuits))
+        circuits = []
+        for i, circuit in enumerate(multi_programming_circuit):
+            pass_manager = multi_tasking_pass_manager(transpile_a)
+            dag = circuit_to_dag(circuit)
+            new_dag = pass_manager.run(dag)
+            circuits.append(dag_to_circuit(new_dag))
+
+        # transpiled_circuits = list(map())
+        if len(circuits) == 1:
+            return circuits[0]
+        return circuits
+
+    transpiled_circuit = transpile(
+        circuits=multi_programming_circuit,
+        backend=backend,
+        backend_properties=backend_properties,
+        optimization_level=3
+    )
+    if len(transpiled_circuit) == 1:
+        return transpiled_circuit[0]
+    return transpiled_circuit
 
 
 def _compose_multicircuits(circuit_config_tuple: Tuple[List[QuantumCircuit], Dict]) -> QuantumCircuit:
@@ -80,35 +83,6 @@ def _compose_multicircuits(circuit_config_tuple: Tuple[List[QuantumCircuit], Dic
     bit_counter = 0
     dag_list = [circuit_to_dag(circuit) for circuit in circuits]
     return dag_to_circuit(_compose_dag(dag_list))
-    # for circuit in circuits:
-    #     register_size = circuit.num_qubits
-
-    #     """FIXME!
-    #     # register_name: register nameを定義すると、outputの `new_dag` に対して `dag_to_circuit()`
-    #     # を実行した時、
-    #     # qiskit.circuit.exceptions.CircuitError: 'register name "定義した名前" already exists'
-    #     # が発生するため、任意のレジスター名をつけることができない
-
-    #     reg_name_tmp = circuit.qubits[0].register.name
-    #     register_name = circuit.qubits[0].register.name if circuit.qubits[
-    #         0].register.name not in name_list else None
-    #     name_list.append(register_name)
-    #     """
-    #     # 上記FIXME部分はNoneで対応中: 2020 / 08 / 16
-    #     register_name = None
-    #     ########################
-
-    #     qr = QuantumRegister(size=register_size, name=register_name)
-    #     cr = ClassicalRegister(size=register_size, name=register_name)
-    #     composed_multicircuit.add_register(qr)
-    #     composed_multicircuit.add_register(cr)
-    #     qubits = composed_multicircuit.qubits[bit_counter:bit_counter+register_size]
-    #     clbits = composed_multicircuit.clbits[bit_counter:bit_counter+register_size]
-    #     composed_multicircuit = composed_multicircuit.compose(
-    #         circuit, qubits=qubits, clbits=clbits)
-
-    #     bit_counter += register_size
-    # return composed_multicircuit
 
 
 def _compose_dag(dag_list):
@@ -159,19 +133,18 @@ def _parse_combine_args(multi_circuits, backend, backend_propaerties, output_nam
         backend_propaerties, backend, num_circuits)
     output_names = _perse_output_names(output_names, num_circuits)
 
-    combine_args = []
+    list_combine_args = []
     for args in zip(backend_properties, output_names):
-        arg_dict = {'num_qubit': len(args[0]['qubits']),
-                    'output_name': args[1],
-                    }
+        combine_args = {'output_name': args[1],
+                        }
         """これから増やす
             'coupling_map': args[?],
             'initial_layout': args[?],
         ...
         """
-        combine_args.append(arg_dict)
+        list_combine_args.append(combine_args)
 
-    return combine_args
+    return list_combine_args
 
 
 def _parse_backend_properties(backend_properties, backend, num_circuits):
@@ -189,11 +162,11 @@ def _perse_output_names(output_names, num_circuits):
     if output_names is None:
         output_names = [None] * num_circuits
     if output_names is not list:
-        if num_circuits != 1:
-            output_names = [output_names+"_" +
+        if num_circuits != 1 and not isinstance(output_names, None):
+            output_names = [str(output_names)+"_" +
                             str(i) for i in range(num_circuits)]
         else:
-            output_names = [output_names]
+            output_names = [None] * num_circuits
     return output_names
 
 
